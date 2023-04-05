6e3cce706e4ecd1b4ce383e4bf46980290de2218
xachan <xachan@s.sfusd.edu>
2023-04-05 10:32:13 -0700

    5.1

--- Assets/Course Library/_Source_Files/Scripts/Target.cs
+++ Assets/Course Library/_Source_Files/Scripts/Target.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        targetRb = GetComponent<Rigidbody>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        Vector3 RandomForce()
+        {
+            return Vector3.up * Random.Range(minSpeed, maxSpeed);
+        }
+
+        float RandomTorque()
+        {
+            return Random.Range(-maxTorque, maxTorque);
+        }
+
+        Vector3 RandomSpawnPos()
+        {
+            return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+        }
+
     }
 }
