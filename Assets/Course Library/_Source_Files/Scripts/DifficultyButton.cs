656010d8d2f5cc5d2d74337722f71bdf8fd9b14a
xachan <xachan@s.sfusd.edu>
2023-04-14 13:21:25 -0700

    5.4

--- Assets/Course Library/_Source_Files/Scripts/DifficultyButton.cs
+++ Assets/Course Library/_Source_Files/Scripts/DifficultyButton.cs
@@ -8,6 +8,8 @@ public class DifficultyButton : MonoBehaviour
     private Button button;
     private GameManager gameManager;
 
+    public int difficulty;
+
 
     // Start is called before the first frame update
     void Start()
--- Assets/Course Library/_Source_Files/Scripts/GameManager.cs
+++ Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -11,17 +11,18 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
-    void StartGame()
+    void Start()
     {
-        
 
 
-       
+
+
     }
 
     // Update is called once per frame
@@ -54,18 +55,19 @@ public class GameManager : MonoBehaviour
     }
 
     public void RestartGame()
-        {
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-    public void StartGame()
+
+    public  void StartGame()
     {
         isGameActive = true;
         score = 0;
         StartCoroutine(SpawnTarget());
-
         UpdateScore(0);
 
+        titleScreen.gameObject.SetActive(false);
+            
     }
-
 }
-
+    
